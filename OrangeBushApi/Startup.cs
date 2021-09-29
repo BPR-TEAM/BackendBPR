e45f0da26534a81105c89b208ab74db55fdddd0c
Nitramzz <navaron2@gmail.com>
2021-09-29 11:47:18 +0200

    Swagger now on production

--- OrangeBushApi/Startup.cs
+++ OrangeBushApi/Startup.cs
@@ -37,11 +37,12 @@ namespace OrangeBushApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrangeBushApi v1"));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OrangeBushApi v1"));
             }
 
             app.UseHttpsRedirection();
