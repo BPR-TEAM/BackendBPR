9e2764e9daa42f26d11467ecbdc08eb47c5af80f
Nitramzz <navaron2@gmail.com>
2021-09-29 13:33:06 +0200

    gorrila controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 namespace BackendBPR.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]🦍")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
