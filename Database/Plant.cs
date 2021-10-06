2a6063fc05d22cc611c47e5d79bd65a7f153e2e5
LuigiElo <42806950+LuigiElo@users.noreply.github.com>
2021-10-06 09:47:42 +0200

    PLant update

--- Database/Plant.cs
+++ Database/Plant.cs
@@ -5,12 +5,14 @@ namespace Database
     public class Plant
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
+        public string CommonName { get; set; } 
+        public string ScientificName { get; set; } 
         public byte[] Image { get; set;}
         public string Url { get; set; }
         public string Description { get; set; }
 
         public virtual ICollection<UserPlant> UserPlants { get; set; }       
         public virtual ICollection<Tag> Tags { get; set; }
+        public virtual ICollection<MeasurementDefinition> MeasurementDefinitions {get;set;}
     }
 }
